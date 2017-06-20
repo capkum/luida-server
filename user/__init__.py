3eef163c646d1efce2e83f4e159ceef0f3641ef7
Seoungjin kim <capnaver@gmail.com>
2017-06-20 15:07:16 +0900

    로그인 시 jwt 토큰 생성
    로그인시 jwt토큰 생성후 결과를 해더로 전송

--- user/__init__.py
+++ user/__init__.py
@@ -1,9 +1,11 @@
 from user.models import User
 from user.models import db
 from flask import Blueprint
-from flask import request
+from flask import request, jsonify
+import jwt
 
 users = Blueprint('user', __name__)
+secret_key = 'hubmedia'
 
 
 @users.route('/signup', methods=['POST'])
@@ -16,9 +18,14 @@ def create():
         createUser = User(_username, _email, _passwd)
         db.session.add(createUser)
         db.session.commit()
-        return 'success'
+
+        rt = jsonify(username=_username,
+                     email=_email, passwd=_passwd,
+                     status='success')
+        return rt
 
     except Exception as e:
+        print(str(e))
         return 'false'
 
 
@@ -28,15 +35,30 @@ def login():
     _passwd = request.values.get('passwd')
 
     try:
-        result = User.query.filter_by(email=_email, passwd=_passwd).all()
+        result = User.query.filter_by(email=_email, passwd=_passwd).first()
 
         # 로그인 결과
-        if len(result) <= 0:
+        if type(result) is 'NoneType':
             return 'login fail'
 
-        return 'login success'
+        create_token = jwt.encode(
+            {
+                'email': result.email,
+                'id': result.id
+            }, secret_key, algorithm='HS256')
 
-    except Exception:
+        json_data = jsonify(
+            email=result.email,
+            username=result.username,
+            status='login success'
+        )
+
+        json_data.headers.add('token', create_token)
+
+        return json_data
+
+    except Exception as e:
+        print(str(e))
         return 'login fail'
 
 
@@ -50,12 +72,20 @@ def signout():
     try:
         _email = request.values.get('email')
         _passwd = request.values.get('passwd')
-        user = User.query.filter_by(email=_email, passwd=_passwd).first()
 
-        if len(user) > 0:
+        user_token = request.headers.get('token')
+        userid = jwt.decode(user_token, secret_key, algorithm='HS256')
+
+        user = User.query.filter_by(id=userid['id'], passwd=_passwd).first()
+
+        if type(user) is not 'NoneType':
             db.session.delete(user)
             db.session.commit()
-            return 'delete success'
+
+            rt = jsonify(email=_email, passwd=_passwd,
+                         status='delete success')
+            return rt
 
     except Exception as e:
+        print(str(e))
         return 'delete fail'
