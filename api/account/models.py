59e784ce4fdce23f1b6a7f2f7044816f12048431
Seoungjin Kim <capnaver@gmail.com>
2017-06-27 23:18:41 +0900

    accounts model 수정
    not null 추가

--- api/account/models.py
+++ api/account/models.py
@@ -8,12 +8,12 @@ import datetime
 
 class Accounts(db.Model, ToDictMixin):
     seq = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(12), nullable=True, doc='이름')
-    email = db.Column(db.String(120), unique=True, nullable=True, doc='email')
-    nickname = db.Column(db.String(20), unique=True,  nullable=True, doc='별명')
-    passwd = db.Column(db.String(64), nullable=True, doc='비밀번호')
+    name = db.Column(db.String(12), nullable=False, doc='이름')
+    email = db.Column(db.String(120), unique=True, nullable=False, doc='email')
+    nickname = db.Column(db.String(20), unique=True, nullable=False, doc='별명')
+    passwd = db.Column(db.String(64), nullable=False, doc='비밀번호')
     device_id = db.Column(db.String(200), doc='안드로이드 아이디')
-    created_at = db.Column(db.DateTime, nullable=True, doc='작성일')
+    created_at = db.Column(db.DateTime, nullable=False, doc='작성일')
 
     def __init__(self, name, email, nickname, passwd, device_id):
         self.name = name
