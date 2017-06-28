dc05188f7105eb35df8f0800e9e4fe1367df1dca
Seoungjin kim <capnaver@gmail.com>
2017-06-28 12:55:12 +0900

    account에 컬럼 추가
    profile_img, voice, point, evnt_point 추

--- api/account/models.py
+++ api/account/models.py
@@ -3,7 +3,6 @@
 from api.database import ToDictMixin
 from api.database import db
 import datetime
-# from common.util import creat_timestamp
 
 
 class Accounts(db.Model, ToDictMixin):
@@ -12,13 +11,22 @@ class Accounts(db.Model, ToDictMixin):
     email = db.Column(db.String(120), unique=True, nullable=False, doc='email')
     nickname = db.Column(db.String(20), unique=True, nullable=False, doc='별명')
     passwd = db.Column(db.String(64), nullable=False, doc='비밀번호')
+    profile_img = db.Column(db.String(255), nullable=True, doc='프로필 사진')
+    voice = db.Column(db.String(120), nullable=True, doc='목소리')
+    point = db.Column(db.Integer, default=0, nullable=True, doc='포인트')
+    evnt_point = db.Column(db.Integer, default=0, nullable=True, doc='이벤트포인트')
     device_id = db.Column(db.String(200), doc='안드로이드 아이디')
     created_at = db.Column(db.DateTime, nullable=False, doc='작성일')
 
-    def __init__(self, name, email, nickname, passwd, device_id):
+    def __init__(self, name, email, nickname, passwd, device_id,
+                 profile_img=None, voice=None, point=0, evnt_point=0):
         self.name = name
         self.email = email
         self.nickname = nickname
         self.passwd = passwd
         self.device_id = device_id
+        self.profile_img = profile_img
+        self.voice = voice
+        self.point = point
+        self.evnt_point = evnt_point
         self.created_at = datetime.datetime.now()
