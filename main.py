acdcede2f83480b0b9c99cac58fbf5d44141b5f9
Seoungjin Kim <capnaver@gmail.com>
2017-08-01 19:04:27 +0900

    test_signin error
    tests/test_signin.py 실행시 luida_server/hooks.py:3 ImportError발생

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from server_api.database import db, redis_db
 from server_api.accounts.views import acnt
 from server_api.auth.views import auth
+from luida_server.hooks import * # noqa
 
 app = Flask(__name__)
 app.config.from_object('luida_server.settings.DevelopmentConfig')
@@ -21,17 +22,6 @@ app.register_blueprint(auth)
 
 # error handler
 from luida_server.error_handler import *  # noqa
-
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers',
-                         'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    response.headers.add('Access-Control-Expose-Headers', 'autho_tk')
-    return response
-
 
 if __name__ == '__main__':
     app.run()
